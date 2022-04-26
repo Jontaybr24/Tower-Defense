d165e34e1983a059241be30b44e85868bb3eb75e
Jontaybr <Jontaybr@gmail.com>
2022-04-26 03:01:02 -0600

    little fix

--- scripts/loader.js
+++ scripts/loader.js
@@ -658,7 +658,7 @@ MyGame.loader = (function () {
                     grid: { label: 'Toggle Grid', key: 'h' },
                     startWave: { label: 'Start Wave', key: 'g' },
                     sell: { label: 'Sell', key: 's' },
-                    upgrade1: { label: 'Upgrade Path 1', key: 'u' },
+                    upgrade1: { label: 'Upgrade Path 1', key: '1' },
                     upgrade2: { label: 'Upgrade Path 2', key: '2' },
                     upgrade3: { label: 'Upgrade Path 3', key: '3' },
                     path: { label: 'Toggle Path', key: 'p' },
